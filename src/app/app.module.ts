e1054a04b7d9b6cc58a506f03c2108d9a559a5ab
lilichenko_hw <lorxop@gmail.com>
2021-03-11 20:53:14 +0200

    demo2

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GenreComponent } from './genre/genre.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { PopularComponent } from './popular/popular.component';
+import { ProfileComponent } from './profile/profile.component';
+
+const appRoutes: Routes = [
+  {path:'', redirectTo: 'home', pathMatch:'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'popular', component: PopularComponent},
+  {path: 'genre', component: GenreComponent},
+  {path: 'profile', component: ProfileComponent},
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GenreComponent,
+    HomeComponent,
+    MenuComponent,
+    PopularComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
